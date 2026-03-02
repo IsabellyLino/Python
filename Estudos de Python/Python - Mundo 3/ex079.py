27cb73ef6ff416097867e60c0c3d90f60ad95c89
IsabellyLino <104798055+IsabellyLino@users.noreply.github.com>
2026-03-02 17:02:23 -0300

    Treinando listas

---
+++ Estudos de Python/Python - Mundo 3/ex079.py
@@ -0,0 +1,13 @@
+#Exercício Python 079: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+valores = []
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in valores:
+        valores.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r == 'N':
+        break
+print(f'Você digitou os valores {sorted(valores)}')
